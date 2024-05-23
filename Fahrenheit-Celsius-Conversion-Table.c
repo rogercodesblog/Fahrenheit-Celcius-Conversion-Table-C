e575f8d0fbcfd79682025172ba568bb87021b6fb
RogerCodes <rogelioherreradev@gmail.com>
2024-05-23 11:32:32 -0600

    replaced while for a for loop, defined constants, made the code more readable.

--- Fahrenheit-Celsius-Conversion-Table.c
+++ Fahrenheit-Celsius-Conversion-Table.c
@@ -1,30 +1,26 @@
+/*Include stdio.h library, so we can use printf*/
 #include <stdio.h>
+
+/*Define constants*/
+#define START 0
+#define END 300
+#define STEP 5
 
 /*Fahrenheit - Celsius Conversion Table */
 main()
 {
-    /*Variables declaration*/
-    int lower, upper, step;
-    float fahrenheit, celsius;
-
-    /*Setting Values*/
-    lower = 0;   /* Lower limit of temperature table (this is where the table will start) */
-    upper = 300; /* Upper limit (Used to stop the while loop) */
-    step = 5;    /* Step size (you can change it as you please) */
-
-    fahrenheit = lower; // set fahrenheit value to 0
-
+    /*Print Messges to console to explain what's going on*/
     printf("This little piece of software was made with the intention of showing how to convert Fahrenheit to Celsius. \n");
-    printf("You'll see a table with its respective equivalents, calculated with the code. \n\n");
+    printf("You'll see a table with its respective equivalents. \n\n");
     printf("Fahrenheit-Celsius Conversion Table \n\n");
 
-    while (fahrenheit <= upper)
+    /*Define variable*/
+    int fahrenheit;
+
+    /*For Loop that will end when the 'fahrenheit' variable reaches the value of END*/
+    for (fahrenheit = START; fahrenheit <= END; fahrenheit = fahrenheit + STEP)
     {
-        /*Formula*/
-        celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
-        /*Print Result to Console*/
-        printf("Fahrenheit: %.2f -- Celsius: %.2f \n", fahrenheit, celsius);
-        /*Fahrenheit values goes up, until it reaches the limit set by the variable 'Upper' and the while loop breaks"*/
-        fahrenheit = fahrenheit + step;
+        /*Print the repective values, the celcius value is calculated then printed*/
+        printf("Fahrenheit: %.2f -- Celsius: %.2f \n", fahrenheit, (5.0 / 9.0) * (fahrenheit - 32.0));
     }
 }
